b646c202620fbe0822d02af9e4475749c080ae30
sawansahrma880 <102312481+sawansahrma880@users.noreply.github.com>
2023-10-13 18:23:57 +0530

    Update page.tsx

--- app/storyes/page.tsx
+++ app/storyes/page.tsx
@@ -9,7 +9,7 @@ function Stories() {
     <Navbar/>
     
 
-    <div className='w-full flex justify-center my-16 font-weedy flex-wrap'>
+    {/* <div className='w-full flex justify-center my-16 font-weedy flex-wrap'>
 
         <div className='md:w-80 flex mt-5 px-4 md:px-0 flex-wrap'>
                     <img className='w-full stories_class ' src="DSC09777.jpg" alt="" />
@@ -32,7 +32,67 @@ function Stories() {
                     <span>8 October 2023</span>
         </div>
 
-    </div>
+    </div> */}
+
+
+<div className="max-w-screen-2xl mx-auto px-4 py-16 lg:py-24 relative bg-gray-50">
+  <div className="flex flex-col md:flex-row gap-2">
+      <div className="flex flex-1 flex-col gap-2">
+          <div className="flex flex-1 flex-col">
+              <img className="object-cover h-full" src="\_64I8739 .jpg"/>
+          </div>
+          <div className="hidden md:flex flex-1 flex-row gap-2">
+              <div className="flex flex-1 flex-col">
+                <img className="object-cover h-full" src="\_64I8742.jpg"/>
+              </div>
+              <div className="hidden md:flex flex-1 flex-col">
+                <img className="object-cover h-full" src="\card_3.jpg"/>
+              </div>
+          </div>
+      </div>
+      <div className="flex flex-1 flex-col gap-2">
+          <div className="hidden md:flex flex-1 flex-row gap-2">
+              <div className="flex flex-1 flex-col">
+                <img className="object-cover h-full" src="\DSC00722.jpg"/>  
+              </div>
+              <div className="hidden md:flex flex-1 flex-col">
+                <img className="object-cover h-full" src="\_A3A7866.jpg"/>
+              </div>
+          </div>                        
+          <div className="flex flex-1 flex-col">
+            <img className="object-cover h-full" src="\DSC00663.jpg"/>
+          </div>
+      </div>
+  </div>
+</div>
+
+
+
+<div className="max-w-screen-2xl mx-auto px-4 py-16 lg:py-24 relative bg-white">
+  <div className="flex flex-col md:flex-row gap-2">
+      <div className="flex flex-1 flex-col">
+          <div className="flex flex-1 flex-col">
+              <img className="object-cover h-full" src='\_A3A8343.jpg' alt=''/>
+          </div>
+      </div>
+      <div className="flex flex-1">
+          <div className="grid grid-cols-2 gap-2">
+            <div>
+              <img className="object-cover h-full" src='\DSC09403.jpg' alt=''/>
+            </div>
+            <div>
+              <img className="object-cover h-full" src='\mandhi.jpg' alt=''/>
+            </div>
+            <div>
+              <img className="object-cover h-full" src='\_64I8825.jpg' alt=''/>
+            </div>
+            <div>
+              <img className="object-cover h-full" src='\DSC00789.jpg' alt=''/>
+            </div>
+          </div>
+      </div>
+  </div>
+</div>
 
     
 
